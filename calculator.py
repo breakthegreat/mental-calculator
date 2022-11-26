1d893e4619be98490dfb886fb28ccb670df55b8a
breakthegreat <breakthegreat@gmail.com>
2022-11-26 00:19:03 -0500

    added a timing feature + minor changes

--- calculator.py
+++ calculator.py
@@ -1,25 +1,32 @@
 import random
-
-def generatingNumbers(min,max):
-  number1 = random.randint(min,max)
-  number2 = random.randint(min,max)
- 
-  return number1, number2
+import time
 
 
-def result(list):
- return list[0] + list[1]
+def generatingNumbers(min, max):
+    number1 = random.randint(min, max)
+    number2 = random.randint(min, max)
+
+    return number1, number2
+
+
+def resultAddition(list):
+    return list[0] + list[1]
 
 
 num1 = int(input("Enter the min number"))
 num2 = int(input("Enter the max number"))
-nums = generatingNumbers(num1,num2)
+nums = generatingNumbers(num1, num2)
 
 
-finalResult = result(nums)
+finalResult = resultAddition(nums)
+initial = time.time()
 answer = ""
 
-while(answer != finalResult):
-  answer = int(input("What is " + str(nums[0]) + " + " + str(nums[1]) + " ?"))
+while (answer != finalResult):
 
-print("You guessed it correctly")
+    answer = int(
+        input("What is " + str(nums[0]) + " + " + str(nums[1]) + " ?"))
+seconds = time.time()
+finalSecond = str(seconds - initial)
+finalSecond = finalSecond[0:4]  # get only up to 2 decimals
+print("You guessed it correctly! it took you " + finalSecond + " seconds")
