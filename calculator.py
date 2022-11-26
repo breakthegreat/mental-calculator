2518650e57895cff7274f3511dc63771dc0c0094
breakthegreat <breakthegreat@gmail.com>
2022-11-26 00:45:14 -0500

    added multiple features, add/sub/div/multi, added an infinite loop that lets the user keep going, fixed formatting issues, TODO: FIX THE DIV METHOD

--- calculator.py
+++ calculator.py
@@ -25,20 +25,59 @@ def resultMulti(list):
     return list[0] * list[1]
 
 
-num1 = int(input("Enter the min number"))
-num2 = int(input("Enter the max number"))
-nums = generatingNumbers(num1, num2)
+num1 = int(input("Enter the min number: "))
+num2 = int(input("Enter the max number: "))
 
+while (True):
+    choosing = True
+    add = False
+    sub = False
+    div = False
+    multi = False
+    while (choosing):
+        nums = generatingNumbers(num1, num2)
+        operation = input(
+            "What type of operation do you want to perfom? (ADD/SUB/MULTI/DIV) or type quit to exit ")
+        if (operation.lower() == "add"):
+            finalResult = resultAdd(nums)
+            choosing = False
+            add = True
+        elif (operation.lower() == "sub"):
+            finalResult = resultSub(nums)
+            choosing = False
+            sub = True
+        elif (operation.lower() == "multi"):
+            finalResult = resultMulti(nums)
+            choosing = False
+            multi = True
+        elif (operation.lower() == "div"):
+            finalResult = resultDiv(nums)  # FIX THIS FLOAT NUMBER !!!
+            choosing = False
+            div = True
+        elif (operation.lower() == "quit"):
+            quit()
+        else:
+            print("Incorrect input please retype ")
 
-finalResult = resultAdd(nums)
-initial = time.time()
-answer = ""
+    initial = time.time()
+    answer = ""
 
-while (answer != finalResult):
+    while (answer != finalResult):
+        if (add):
+            answer = int(
+                input("What is " + str(nums[0]) + " + " + str(nums[1]) + " ? "))
+        elif (sub):
+            answer = int(
+                input("What is " + str(nums[0]) + " - " + str(nums[1]) + " ? "))
+        elif (div):
+            answer = int(
+                input("What is " + str(nums[0]) + " / " + str(nums[1]) + " ? "))
+        elif (multi):
+            answer = int(
+                input("What is " + str(nums[0]) + " * " + str(nums[1]) + " ? "))
 
-    answer = int(
-        input("What is " + str(nums[0]) + " + " + str(nums[1]) + " ?"))
-seconds = time.time()
-finalSecond = str(seconds - initial)
-finalSecond = finalSecond[0:4]  # get only up to 2 decimals
-print("You guessed it correctly! it took you " + finalSecond + " seconds")
+        # TODO ADD MORE INPUT ACCORDING TO EACH OPERATION CHOSED
+    seconds = time.time()
+    finalSecond = str(seconds - initial)
+    finalSecond = finalSecond[0:4]  # get only up to 2 decimals
+    print("You guessed it correctly! it took you " + finalSecond + " seconds")
